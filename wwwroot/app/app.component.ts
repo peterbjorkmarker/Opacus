d71b525d43118863f7f420f3f4526183d423aa69
Peter Bjorkmarker <peter.bjorkmarker@live.com>
2016-10-09 22:12:00 +0200

    Initial commit

---
+++ wwwroot/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { PersonService, Person } from './person.service';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>Opacus</h1>
+    <ul>
+     <li *ngFor="let person of persons">
+    <strong>{{person.name}}</strong><br>
+    from: {{person.city}}<br>
+    date of birth: {{person.dob | date: 'dd/MM/yyyy'}}
+    </li>
+    </ul>
+    `,
+    providers: [
+        PersonService
+    ]
+})
+export class AppComponent extends OnInit {
+
+    constructor(private _service: PersonService) {
+        super();
+    }
+
+    ngOnInit() {
+        this._service.loadData().then(data => {
+            this.persons = data;
+        })
+    }
+
+    persons: Person[] = [];
+}
